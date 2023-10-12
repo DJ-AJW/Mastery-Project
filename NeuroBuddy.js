8e4ff9a7725f1e301ceeda64ad5e4b4a0c2dbb40
Mdza-4 <147776339+Mdza-4@users.noreply.github.com>
2023-10-12 16:32:40 -0700

    Update NeuroBuddy.js

--- NeuroBuddy.js
+++ NeuroBuddy.js
@@ -1,39 +1,67 @@
+var screen = 0; //main menu
 let button;
 let img;
 
-function preload() {
-  img = loadImage('brain-1.png');
-}
 
 function setup() {
   createCanvas(1536, 707);
 
-  button = createButton('Start!');
-  button.position((2 * width / 3) - 175, (13 * height / 16) - 150);
-  button.size(200, 125);
-  button.mousePressed(changeWindow);
+  button = createButton("Start!");
+  button.position((2 * width) / 3 - 200, (13 * height) / 16 - 300);
+  button.size(400, 150);
+  button.mousePressed(startGame);
 }
 
 function draw() {
-  background(104,199,255);
-  
-  fill(0,75,132);
+  if (screen == 0){
+    menuScreen();
+  }
+  else if(screen == 1){
+    reactionTime();
+  }
+}
+function menuScreen(){
+  background(104, 199, 255);
+
+  fill(0, 75, 132);
   textStyle(BOLD);
   textAlign(CENTER);
   textSize(100);
-  text("NeuroBuddy", 2 * width / 3, height / 3);
-  
-  textSize(32);
-  text("Assisting your brain in the recovery\n process", 2 * width / 3, 3 * height / 4);
-  
-  tint(104,199,255);
-  image(img, width / 6, height / 3, width / 4, height / 2);
-  
-  rect((2 * width / 3) - 175, (13 * height / 16) - 150, 200, 125);
-}
+  text("NeuroBuddy", (2 * width) / 3, height / 2);
 
-function changeWindow(){
+  textSize(32);
+  text(
+    "Assisting your brain in the recovery\n process",
+    (2 * width) / 3,
+    (3 * height) / 4
+  );
+
+  tint(104, 199, 255);
+
+  rect((2 * width) / 3 - 200, (13 * height) / 16 - 300, 400, 150);
+}
+function reactionTime(){
+  //rectangle
+  background(0,200,250);
+  fill(0,0,255);
+  rect(width/7,200,500,300);
+  //text
   fill(255);
-  rect(0,0,1536, 707);
-  
+  textFont("Arial");
+  textSize(38);
+  text("Click The Blue Box Once It Turns Green!",width/4,height/7);
+  //word box
+  noFill();
+  rect(200,550,350,100);
+  fill(255);
+  text("ms",450,610);
+  button.style.display = 'none';
+}
+function mousePressed(){
+  if (screen == 0){
+    startGame();
+  }
+}
+function startGame(){
+  screen = 1;
 }
